ce72cdee42fcef0e1c664d9099a09b564e6ba2b4
Agam Jain <jainagam4@gmail.com>
2024-08-05 01:00:27 +0530

    first commit

---
+++ simple-prototype/src/componenets/Video_editing.jsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+const steps = [
+  {
+    label: 'Choose Your Device: Phone, Tablet, or Computer',
+    description: `There are two kinds of softwares available : Paid and Free(choose)
+                1st Kind: chose Paid one direct to the next step.
+                3nd Kind : Kind of people they have to use paid with the tricks legal direct way and if its illegal have to get another ways ie.Hints to Direct him to the right source
+              `,
+  },
+  {
+    label: 'Computer',
+    description:
+      'Firstly we can help them to buid the environment by the related tags that are usefull to them',
+  },
+  {
+    label: 'Action',
+    description: `So First we need to look into urgency and quality of the output that the user needed to look and thing is maybe we can suggest the time and effort in deciding wheather to choose online mode or in built software(we need the user to decide wheather whats good for him by the stats of the software the useablity of the software :`,
+  },
+];
+
+export default function VerticalLinearStepper() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel
+              optional={
+                index === 2 ? (
+                  <Typography variant="caption">Last step</Typography>
+                ) : null
+              }
+            >
+              {step.label}
+            </StepLabel>
+            <StepContent>
+              <Typography>{step.description}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Back
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </Box>
+  );
+}
